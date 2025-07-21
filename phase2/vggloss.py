92c332d178cc9e9e99053e3a53f46dec0efcc329
mukesh reddy p <88029886+pmukeshreddy@users.noreply.github.com>
2025-07-21 14:27:29 -0400

    Create vggloss.py

---
+++ phase2/vggloss.py
@@ -0,0 +1,17 @@
+from torchvision import models
+
+class VGGPerceptualLoss(nn.Module):
+    def __init__(self):
+        super(VGGPerceptualLoss, self).__init__()
+        vgg = models.vgg19(weights=models.VGG19_Weights.DEFAULT).features
+        self.vgg_layers = nn.Sequential(*[vgg[i] for i in range(36)]).eval()
+        for param in self.vgg_layers.parameters():
+            param.requires_grad = False
+        self.loss = nn.L1Loss()
+
+    def forward(self, generated, target):
+        generated_norm = (generated + 1) / 2
+        target_norm = (target + 1) / 2
+        vgg_generated = self.vgg_layers(generated)
+        vgg_target = self.vgg_layers(target)
+        return self.loss(vgg_generated, vgg_target)
